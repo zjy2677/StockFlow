c9de1806df9db45943ad09ec13f8f6afd047c0af
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-18 18:03:37 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,14 @@
+from __future__ import annotations
+from fastapi import FastAPI, HTTPException
+from backend.service import(
+    MovementRequest,
+    MovementResponse,
+    StockResponse,
+    StockService,
+)
 
+# create a fastapi object app with tile and version
+app = FastAPI(title="StockFlow API", version="1.0.0")
+
+# create a StockService obkect
+service = StockService()
