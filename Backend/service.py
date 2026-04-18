cb8eb7e47ca0e3c44c4da96011f6a58ad789e665
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-18 16:43:25 +0200

    Update service.py

--- Backend/service.py
+++ Backend/service.py
@@ -4,7 +4,22 @@ from fastapi import HTTPException
 from pydantic import BaseModel, Field, field_validator
 
 class MovementRequest(BaseModel):
-  message: str
-  product_id: str
-  quantity: str
+  # No default set to product_id but it has to conatin at least 1 character
+  product_id = Field(...,min_length=1)
+  # No default set to quantity but it has to be greater than or equal to 0
+  quantity = Field(...,ge=0)
+  # "type" is either in/out defined as string 
+  type: str
+
+  # Validator for the product_id
+  @field_validator("product_id")
+  @classmethod
+  def validate_product_id(cls, value: str) -> str:
+    clean_id = value.strip()
+    if not clean_id:
+      raise ValueError("product_id must be a non-empty string")
+    return clean_id
+
+
+
 
