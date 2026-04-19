525c3e108aca683b1d0e17b288af9d4022f017d3
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-19 15:54:38 +0200

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -21,8 +21,8 @@ def set_bg(img_path):
         <style>
         .stApp {{
             background: linear-gradient(
-                    rgba(255, 255, 255, 0.24),
-                    rgba(255, 255, 255, 0.23)
+                    rgba(255, 255, 255, 0.32),
+                    rgba(255, 255, 255, 0.32)
                 ),
                 url("data:image/png;base64,{encoded}");
             background-size: cover;
