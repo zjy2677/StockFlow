3708265403069af4ba7f7f600c7089dcc3a25fdd
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-20 22:46:54 +0200

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
-import sys
 import os
 from pathlib import Path
 import streamlit as st
-from fastapi import HTTPException
 import base64
-from pydantic import ValidationError
 
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
-sys.path.append(str(ROOT_DIR))
-
 API_BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
-
 IMG_PATH = ROOT_DIR / "Frontend" / "photo" / "background_img.png"
 
 
